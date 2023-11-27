a974decfba053a2fa8c1f9fec54abec5a476fe77
Danifanton <danielrxd0912@gmail.com>
2023-11-27 17:51:56 -0400

    Trying to fetch an url for scrapping

--- api/app.js
+++ api/app.js
@@ -6,6 +6,8 @@ const routerApi = require('./routes');
 const { errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 const setupAuthStrategies = require('./auth');
 
+const https = require('https');
+
 function createApp() {
   const app = express();
 
@@ -17,6 +19,26 @@ function createApp() {
   app.get('/', (req, res) => {
     res.send('Hola mi server en express');
   });
+
+  app.get('/dolar', async (req, res) => {
+    const handler = new https.Agent({
+      rejectUnauthorized: false,
+    });
+    console.log(handler);
+    try {
+      console.log('trying');
+      const info = await fetch("https://www.bcv.org.ve/", {
+        method: "GET",
+        headers: {
+          "Content-type": "text/hmtl",
+        },
+        handler,
+      });
+      res.send(info);
+    } catch (error) {
+      console.log(error);
+    }
+  })
 
   // app.get('/nueva-ruta', checkApiKey, (req, res) => {
   //   res.send('Hola, soy una nueva ruta');
